e3ceb7fea8b64815898ab825525f32d7bc0fdf74
Jerry Bennett <jerrysbusiness@gmail.com>
2017-11-13 01:45:23 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,15 +10,3 @@ def key_for_min_value(name_hash)
   end
   return hashes[0][0][0]
 end
-
-name_hash.values.each.with_index do |k, v|
-  i = 1000
-  j = 0
-  while j < name_hash.length
-    if v < i
-      i = v
-    end
-    j += 1
-  end
-  return i
-end
