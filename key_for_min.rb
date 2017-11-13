1e95e4bb034ab6e720de11b50c0e11e279890218
Jerry Bennett <jerrysbusiness@gmail.com>
2017-11-13 04:25:09 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,31 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  lowest = Float::INFINITY
+  hashes = []
   if block_given?
-  name_hash.each_with_index do |(key, value), i|
-    value = name_hash[value]
-    j = 0
-    while j < name_hash.length
-      if value < lowest
-        lowest = value
-        j +=
-      end
-      if lowest == name_hash[value]
-        return key
-      end
-    end
+    hashes << name_hash.sort_by {|k, v| v}
   end
+  return hashes[0][0][0]
 end
-
-
-
-  # max_value = 0  # or -Infinity if you have negative values
-  # key_for_max_value = nil
-  #
-  # hash.each_pair do | key, value |
-  #   if value > max_value
-  #     max_value = value
-  #     key_for_max_value = key
-  #   end
-  # end
