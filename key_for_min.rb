84975261c4885aeb248007413626150805d71dc1
Jerry Bennett <jerrysbusiness@gmail.com>
2017-11-13 06:01:19 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,11 +9,10 @@ def key_for_min_value(name_hash)
 
   name_hash.each_pair do |k, v|
     v.each do |n, a|
-
-    end
-    if v < lowest
-      lowest = v
-      return k
+      if v < lowest
+        lowest = v
+        return n
+      end
     end
   end
 end
