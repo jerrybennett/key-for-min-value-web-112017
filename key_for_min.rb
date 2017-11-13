e7d4897ee7513da15b84d42031d4b942f5ecaad0
Jerry Bennett <jerrysbusiness@gmail.com>
2017-11-13 04:51:50 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,7 +10,9 @@ def key_for_min_value(name_hash)
   name_hash.each_pair do |k, v|
     if v < lowest
       lowest = v
-      return k
+      if lowest == v
+        return k
+      end
     end
   end
 end
