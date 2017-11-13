94a9ddf43debbe528406c4b5d30f7e44b67c2198
Jerry Bennett <jerrysbusiness@gmail.com>
2017-11-13 04:43:53 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,14 +5,13 @@ def key_for_min_value(name_hash)
   hashes = []
   if name_hash.empty?
     return nil
-  else
-    lowest = Float::INFINITY
-    low_key = nil
-    name_hash.each_pair do |k, v|
-      if v < lowest
-        lowest = v
-        low_key = k
-      end
+  end
+  lowest = Float::INFINITY
+  low_key = nil
+  name_hash.each_pair do |k, v|
+    if v < lowest
+      lowest = v
+      low_key = k
     end
   end
 end
