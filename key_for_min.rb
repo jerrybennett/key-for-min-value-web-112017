9a0002cdca5873add6f803aa03672f7ca43e218a
Jerry Bennett <jerrysbusiness@gmail.com>
2017-11-13 04:12:23 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,31 +2,29 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  hashes = []
-  if name_hash.empty?
-    return nil
-  else
-    hashes << name_hash.sort_by {|k, v| v}
-  end
-  return hashes[0][0][0]
-end
+  lowest = Float::INFINITY
 
-lowest = Float::INFINITY
-
-name_hash.each_pair do |key, value|
-  if value < lowest
-    lowest = value
-  else
-end
-
-
-
-  max_value = 0  # or -Infinity if you have negative values
-  key_for_max_value = nil
-
-  hash.each_pair do | key, value |
-    if value > max_value
-      max_value = value
-      key_for_max_value = key
+  name_hash.each_with_index do |(key, value), i|
+    value = name_hash[key]
+    for i in name_hash.length
+      if value < lowest
+        lowest = value
+      end
+      if lowest == name_hash[value]
+        return key
+      end
     end
   end
+end
+
+
+
+  # max_value = 0  # or -Infinity if you have negative values
+  # key_for_max_value = nil
+  #
+  # hash.each_pair do | key, value |
+  #   if value > max_value
+  #     max_value = value
+  #     key_for_max_value = key
+  #   end
+  # end
